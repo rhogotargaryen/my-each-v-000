eb747f93fd14252ccc53d89327682081e75258be
Derek Haller <dhaller.personal@gmail.com>
2018-01-25 07:58:57 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -4,11 +4,11 @@ def my_each(array)
 
     while i < array.length
       yield(array[i])
-      i++
+      i = i + 1
     end
 
     array
   else
-    "Hey! No block was given!"
+    puts "Hey! No block was given!"
   end
 end

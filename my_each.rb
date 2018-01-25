246c13e3b1e3e0cc7981a9e10f34d131e270b488
Derek Haller <dhaller.personal@gmail.com>
2018-01-25 07:52:45 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)# put argument(s) here
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i++
+    end
+  else
+    puts "Hey! No block was give"
+  end
 end
